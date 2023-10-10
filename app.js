33fe8ab271e3ac53ad11f38870f35d440e99071e
samuel21695 <samuel21695@gmail.com>
2023-10-10 22:50:31 +0900

    third function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 setTimeout(function() {
   console.log("첫번째 함수");
   setTimeout(function() {
-    console.log("두번쨰 함수");
-  }, 1000)
-}, 1000)
+    console.log("두번째 함수");
+    setTimeout(function() {
+      console.log("세번째 함수");
+    }, 1000);
+  }, 1000);
+}, 1000);
