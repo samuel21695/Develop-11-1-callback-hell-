3418bdcfd5a9bb14e701b191a64c342e1c62d756
samuel21695 <samuel21695@gmail.com>
2023-10-10 22:51:32 +0900

    fourth function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ setTimeout(function() {
     console.log("두번째 함수");
     setTimeout(function() {
       console.log("세번째 함수");
+      setTimeout(function() {
+        console.log("네번째 함수");
+      }, 1000);
     }, 1000);
   }, 1000);
 }, 1000);
